07343f685dbb0858f5b32a1d51fa59b916dbded1
AdutlaSaiKrishnaReddy <saitej7396@gmail.com>
2026-01-29 22:41:17 +0530

    Fix duplicate constructor in LoginResponse

--- src/main/java/com/eventsphere/dto/LoginResponse.java
+++ src/main/java/com/eventsphere/dto/LoginResponse.java
@@ -9,13 +9,6 @@ public class LoginResponse {
     private String token;
     private String role;
 	
-	public LoginResponse(String token, String role) {
-		super();
-		this.token = token;
-		this.role = role;
-	}
-	
-	
 	public String getToken() {
 		return token;
 	}
